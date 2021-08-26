848c222ba64bb61427c4d02ff8b80a418cf16ddb
SIGSEGV-666 <fzerowipeoutlover1998@gmail.com>
2021-08-26 17:26:46 -0600

    added a strided memset to dj_memutils.c

--- dj_memutils/dj_memutils.c
+++ dj_memutils/dj_memutils.c
@@ -10,3 +10,18 @@
 
 //legally do byte-precision addition & subtraction on void pointers on all compilers!
 inline void* djmem_vpoffs(void* og, ssize_t offs){return (void*)(((char*)og)+offs);}
+
+//a strided memcpy/memset
+void* djmem_strided_memset(void* dst, void* src, size_t dstsize, size_t srcsize)
+{
+    unsigned char *ucdst = dst, *ucsrc = src, *ucend; ucend = ucdst+dstsize;
+    uintptr_t bytes_remaining, bytes2copy;
+    while (ucdst < ucend)
+    {
+        bytes_remaining = (uintptr_t)(ucend-ucdst);
+        bytes2copy = (bytes_remaining >= srcsize) ? (srcsize) : (bytes_remaining);
+        memcpy(ucdst, src, bytes2copy);
+        ucdst += bytes2copy;
+    }
+    return dst;
+}
